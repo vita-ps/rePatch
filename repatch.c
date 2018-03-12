0f7f413908329002294cca168e3672f09457f618
dots-tb <dots_tb@gmx.com>
2018-03-12 02:31:46 -0700

    Fixed compatibility with Taiko V and prob other things

--- repatch.c
+++ repatch.c
@@ -12,8 +12,8 @@
 
 #define printf ksceDebugPrintf
 
-static int hooks_uid;
-static tai_hook_ref_t ref_hooks;
+static int hooks_uid[2];
+static tai_hook_ref_t ref_hooks[2];
 
 
 static int ksceIoOpenForPid_patched(SceUID pid, const char *filename, int flag, SceIoMode mode) {
@@ -29,22 +29,52 @@ static int ksceIoOpenForPid_patched(SceUID pid, const char *filename, int flag, 
 			if(old_path[0] == '/')
 				old_path++;
 			snprintf(new_path, sizeof(new_path), "ux0:/rePatch/%s/%s",titleid,old_path);
+			printf("new path %s\n",new_path);
 			ret = ksceIoOpen(new_path, flag, mode);
 		}
 			
 	}
-	if(ret < 0) ret = TAI_CONTINUE(int, ref_hooks, pid, filename, flag, mode);
+	if(ret < 0) ret = TAI_CONTINUE(int, ref_hooks[0], pid, filename, flag, mode);
+	EXIT_SYSCALL(state);
+	return ret;
+}
+static int _sceIoGetstat_patched(const char *file, SceIoStat *stat, int r2) {
+	int ret = -1, state;
+	SceIoStat k_stat;
+	char filename[256];
+	ksceKernelStrncpyUserToKernel(&filename, (uintptr_t)file, sizeof(filename));
+	ENTER_SYSCALL(state);
+	if (memcmp("app0:", filename, sizeof("app0:") - 1)==0) {
+		char new_path[128];
+		char titleid[32];
+		ret = ksceKernelGetProcessTitleId(ksceKernelGetProcessId(), titleid, sizeof(titleid));
+		if(ret > -1) {
+			strncat(new_path, titleid, sizeof(titleid));
+			char *old_path = strchr(filename, ':') + 1;
+			if(old_path[0] == '/')
+				old_path++;
+			snprintf(new_path, sizeof(new_path), "ux0:/rePatch/%s/%s",titleid,old_path);
+			ret = ksceIoGetstat(new_path, &k_stat);
+			if(ret > -1) {
+				ksceKernelMemcpyKernelToUser((uintptr_t)stat, &k_stat, sizeof(k_stat));
+			}
+		}
+
+	}
+	if(ret < 0) ret = TAI_CONTINUE(int, ref_hooks[1], file, stat, r2);
 	EXIT_SYSCALL(state);
 	return ret;
 }
 
 void _start() __attribute__ ((weak, alias ("module_start")));
 int module_start(SceSize argc, const void *args) {
-	hooks_uid = taiHookFunctionExportForKernel(KERNEL_PID, &ref_hooks, "SceIofilemgr", TAI_ANY_LIBRARY, 0xC3D34965, ksceIoOpenForPid_patched);
+	hooks_uid[0] = taiHookFunctionExportForKernel(KERNEL_PID, &ref_hooks[0], "SceIofilemgr", TAI_ANY_LIBRARY, 0xC3D34965, ksceIoOpenForPid_patched);
+	hooks_uid[1] = taiHookFunctionExportForKernel(KERNEL_PID, &ref_hooks[1], "SceIofilemgr", TAI_ANY_LIBRARY, 0x8E7E11F2, _sceIoGetstat_patched);
 	return SCE_KERNEL_START_SUCCESS;
 }
 
 int module_stop(SceSize argc, const void *args) {
-	if (hooks_uid >= 0) taiHookReleaseForKernel(hooks_uid, ref_hooks);   
+	if (hooks_uid[0] >= 0) taiHookReleaseForKernel(hooks_uid[0], ref_hooks[0]);   
+	if (hooks_uid[1] >= 0) taiHookReleaseForKernel(hooks_uid[1], ref_hooks[1]);   
 	return SCE_KERNEL_STOP_SUCCESS;
 }
